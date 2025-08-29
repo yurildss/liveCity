12a98e7739aad70bdd4ff6a439c8445e02944acf
yuri <yurildss@hotmail.com>
2025-08-29 19:10:55 -0300

    Refactor: Clean up imports in MyReviewsScreen.kt
    This commit removes an unused import `androidx.compose.foundation.layout.height` from `MyReviewsScreen.kt`.

--- app/src/main/java/com/example/livecity/screens/myReviews/MyReviewsScreen.kt
+++ app/src/main/java/com/example/livecity/screens/myReviews/MyReviewsScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
