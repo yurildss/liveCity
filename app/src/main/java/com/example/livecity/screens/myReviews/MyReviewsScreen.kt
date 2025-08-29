dc8310fcbf1f77344946d73bcf8279acd68d4474
yuri <yurildss@hotmail.com>
2025-08-29 19:10:33 -0300

    Feat: Initial layout for MyAlertsScreen and AlertsCard
    This commit introduces the initial UI structure for the "My Alerts" screen and a reusable "AlertsCard" composable.
    
    Key changes:
    - **MyReviewsScreen.kt:**
        - Created `MyAlertsScreen` composable function (currently empty).
        - Created `AlertsCard` composable function:
            - Displays a `Column` with padding and size constraints.
            - Contains a `Row` with a `Text` element for "Car Accident" and an `Icon` representing a car crash.
        - Added `AlertsCardPreview` composable function to enable previewing the `AlertsCard` in Android Studio.

--- app/src/main/java/com/example/livecity/screens/myReviews/MyReviewsScreen.kt
+++ app/src/main/java/com/example/livecity/screens/myReviews/MyReviewsScreen.kt
@@ -1,6 +1,21 @@
 package com.example.livecity.screens.myReviews
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.livecity.R
 
 @Composable
 fun MyAlertsScreen(){
@@ -14,5 +29,28 @@ fun AlertsList(){
 
 @Composable
 fun AlertsCard(){
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxHeight(0.85f)
+            .fillMaxWidth(0.75f),
+        horizontalAlignment = Alignment.Start,
+        ) {
+        Row {
+            Text(
+                text = "Car Accident"
+            )
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.car_crash_50dp_ea3323_fill0_wght400_grad0_opsz48),
+                contentDescription = "Car crash icon"
+            )
+        }
 
+    }
+}
+
+@Composable
+@Preview
+fun AlertsCardPreview(){
+    AlertsCard()
 }
